719ef711521749eb1fa28dbf57e33d8532fc5b9f
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-08-18 21:49:55 +0100

    Added initial config and helper js files

--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,6 @@
 import { async } from "regenerator-runtime";
+import { API_URL } from "./config";
+import { getJSON } from "./helpers";
 
 export const state = {
     recipe: {},
@@ -7,12 +9,7 @@ export const state = {
 export const loadRecipe = async function(id) {
     try {
 
-        const response = await fetch(
-        `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-        );
-        const data = await response.json();
-
-        if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+        const data = await getJSON(`${API_URL}/${id}`);
 
         const { recipe } = data.data;
         state.recipe = {
