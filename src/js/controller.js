c56582354b35da9e245154ee92df569a746c93e3
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-08-22 22:07:04 +0100

    Added parent class View.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,10 +1,15 @@
 import * as model from "./model";
 import recipeView from "./views/recipeView";
 import searchView from "./views/searchView";
+import resultsView from "./views/resultsView";
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+import { async } from "regenerator-runtime";
 
+if(module.hot) {
+    module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -32,11 +37,15 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function() {
     try {
+      resultsView.renderSpinner();
+    
       const query = searchView.getQuery();
       if (!query) return;
 
       await model.loadSearchResults(query); 
-      console.log(model.state.search.results); 
+      
+      resultsView.render(model.state.search.results);
+
     } catch(err) {
         console.log(err);
     }
